7113781298e954bee802068fcb47b7ca93b620b3
Lyndon Rakusen <mail@lyndonrakusen.com>
2026-05-23 18:43:50 -0700

    changed featured track. Added new background image.

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,9 +36,14 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'images/bg01.jpg': 'center',
-							'images/bg02.jpg': 'center',
-							'images/bg03.jpg': 'center'
+							'/images/bg01.webp': 'center',
+							'/images/bg02.webp': 'center',
+							'/images/bg03.webp': 'center',
+                            '/images/bg04.webp': 'center',
+                            '/images/bg05.webp': 'center',
+                            '/images/bg06.webp': 'center',
+                            '/images/bg07.webp': 'center',
+                            '/images/bg08.webp': 'center',
 						},
 
 					// Delay.
